ffd010c11981611bd10627d76dfb914b4f3e96f7
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-06-20 18:32:14 -0400

    building game logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-tictactoe
+
+A simple tic-tac-toe application written in Python 3
--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,11 @@
+# Triple A-level game here, folks. Activision Blizzard, Bethesda, Valve... hit me up, let's make a deal.
+
 board = ["-", "-", "-", 
         "-", "-", "-", 
         "-", "-", "-"]
 
 def display_board():
+    
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
@@ -14,9 +17,18 @@ def play_game():
     print("|||||||||||")
     display_board()
 
-    handle_turn()
+    while game_still_going:
+
+    handle_turn(current_player)
+
+    check_if_game_over()
+
+    flip_player()
 
 def handle_turn():
     position = input("Choose a position from 1 - 9: ")
+    position = int(position) - 1
+    board[position] = "X"
+    display_board()
 
 play_game()
