14fcb0c63420099af1b47c59fb1f88f5d590d391
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-06-20 20:24:45 -0400

    tie login, invalid input fix, player override fix

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python-tictactoe
 
-A simple tic-tac-toe application written in Python 3
+A simple tic-tac-toe application written in Python 3.
--- tictactoe.py
+++ tictactoe.py
@@ -15,17 +15,10 @@ winner = None
 current_player = "X"
 
 #Display the board!
-def display_board():
-    
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print(board[6] + " | " + board[7] + " | " + board[8])
+
 
 #Play the game, duh.
 def play_game():
-    print("|||||||||||")
-    print("Game started!")
-    print("|||||||||||")
     display_board()
 
     #While the game is still going... obviously...
@@ -40,18 +33,33 @@ def play_game():
         # Flip to the other player
         flip_player()
 
-        if winner == "X" or winner == "O":
-            print(winner + "won!")
-        elif winner == None:
-            print("Tie!")
+    if winner == "X" or winner == "O":
+        print(winner + " won!")
+    elif winner == None:
+        print("Tie!")
         
-
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
 
 def handle_turn(player):
+    print(player + "'s turn.")
     position = input("Choose a position from 1 - 9: ")
-    position = int(position) - 1
-    board[position] = "X"
+
+    valid = False
+    while not valid:
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Invalid input. Please select a position from 1 - 9: ")
+        position = int(position) - 1
+
+        if board[position] == "-":
+            valid = True 
+        else: 
+            print("You can't choose that spot. Go again.")
+
+    board[position] = player
     display_board()
 
 def check_if_game_over():
@@ -69,23 +77,25 @@ def check_for_winner():
     diagonal_winner = check_diagonals()
 
     if row_winner:
-        winner = row_winner()
+        winner = row_winner
     elif column_winner: 
-        winner = column_winner()
+        winner = column_winner
     elif diagonal_winner:
-        winner = diagonal_winner()
+        winner = diagonal_winner
     else:
         winner = None
-    
 
     return
+    
+
+    
 
 def check_rows():
     global game_still_going
     # Check if any of the rows have the same value
-    row_1 = board[0] == board[1] == [2] != "-"
-    row_2 = board[3] == board[4] == [5] != "-"
-    row_3 = board[6] == board[7] == [8] != "-"
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
     # If any row has a match, flag as win
     if row_1 or row_2 or row_3:
         game_still_going = False
@@ -95,13 +105,15 @@ def check_rows():
         return board[3]
     elif row_3:
         return board[6]
-    return
+    else:
+        return None
+    
 
 def check_columns():
     global game_still_going
-    column_1 = board[0] == board[3] == [6] != "-"
-    column_2 = board[1] == board[4] == [7] != "-"
-    column_3 = board[2] == board[5] == [8] != "-"
+    column_1 = board[0] == board[3] == board[6] != "-"
+    column_2 = board[1] == board[4] == board[7] != "-"
+    column_3 = board[2] == board[5] == board[8] != "-"
     # If any row has a match, flag as win
     if column_1 or column_2 or column_3:
         game_still_going = False
@@ -111,12 +123,13 @@ def check_columns():
         return board[1]
     elif column_3:
         return board[2]
-    return
+    else:
+        return None
 
 def check_diagonals():
     global game_still_going
-    diagonal_1 = board[0] == board[4] == [8] != "-"
-    diagonal_2 = board[2] == board[4] == [6] != "-"
+    diagonal_1 = board[0] == board[4] == board[8] != "-"
+    diagonal_2 = board[2] == board[4] == board[6] != "-"
     # If any row has a match, flag as win
     if diagonal_1 or diagonal_2:
         game_still_going = False
@@ -124,12 +137,23 @@ def check_diagonals():
         return board[0]
     elif diagonal_2: 
         return board[2]
-    return
+    else:
+        return None
 
 def check_if_tie():
+    global game_still_going
+    if "-" not in board:
+        game_still_going = False
     return
 
 def flip_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+        return
+    if current_player == "O":
+        current_player = "X"
+        return
     return
 
 play_game()
